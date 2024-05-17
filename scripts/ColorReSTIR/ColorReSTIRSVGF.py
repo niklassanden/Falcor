ffa9efb134d747e516e5260b199ff39813dd64f5
Niklas Sandén <sandenniklas@gmail.com>
2024-05-17 17:53:23 +0200

    Update ColorReSTIRSVGF RenderGraph

--- scripts/ColorReSTIR/ColorReSTIRSVGF.py
+++ scripts/ColorReSTIR/ColorReSTIRSVGF.py
@@ -4,7 +4,7 @@ from falcor import *
 def render_graph_ColorReSTIRSVGF():
     g = RenderGraph('ColorReSTIRSVGF')
     g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Aces', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
-    g.create_pass('ColorReSTIR', 'ColorReSTIR', {'gOutputMode': 'Combined', 'gCandidateCount': 1, 'gCandidatesVisibility': False, 'gMaxConfidence': 20, 'gTemporalReuse': True, 'SPATIAL_REUSE': 1, 'gMaxSpatialSearch': 10, 'gSpatialRadius': 20, 'maxBounces': 3, 'computeDirect': True, 'useImportanceSampling': True})
+    g.create_pass('ColorReSTIR', 'ColorReSTIR', {'gOutputMode': 'Combined', 'gAnalyticalSamples': 4, 'gEnvironmentSamples': 4, 'gEmissiveSamples': 4, 'gDeltaSamples': 1, 'gCandidatesVisibility': False, 'gMaxConfidence': 20, 'gTemporalReuse': True, 'SPATIAL_REUSE': 1, 'gMaxSpatialSearch': 10, 'gSpatialRadius': 20, 'maxBounces': 3, 'computeDirect': True, 'useImportanceSampling': True})
     g.create_pass('AccumulatePass', 'AccumulatePass', {'enabled': False, 'outputSize': 'Default', 'autoReset': True, 'precisionMode': 'Single', 'maxFrameCount': 0, 'overflowMode': 'Stop'})
     g.create_pass('GBufferRT', 'GBufferRT', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'texLOD': 'Mip0', 'useTraceRayInline': False, 'useDOF': True})
     g.create_pass('GBufferRaster0', 'GBufferRaster', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back'})
@@ -19,12 +19,12 @@ def render_graph_ColorReSTIRSVGF():
     g.add_edge('GBufferRT.posW', 'ColorReSTIR.posW')
     g.add_edge('GBufferRaster0.pnFwidth', 'SVGFPass.PositionNormalFwidth')
     g.add_edge('ColorReSTIR.color', 'SVGFPass.Color')
-    g.add_edge('GBufferRaster0.emissive', 'SVGFPass.Emission')
     g.add_edge('GBufferRaster0.linearZ', 'SVGFPass.LinearZ')
     g.add_edge('ColorReSTIR.albedo', 'SVGFPass.Albedo')
-    g.add_edge('GBufferRaster0.posW', 'SVGFPass.WorldPosition')
-    g.add_edge('GBufferRaster0.guideNormalW', 'SVGFPass.WorldNormal')
-    g.add_edge('GBufferRaster0.mvec', 'SVGFPass.MotionVec')
+    g.add_edge('GBufferRT.emissive', 'SVGFPass.Emission')
+    g.add_edge('GBufferRT.posW', 'SVGFPass.WorldPosition')
+    g.add_edge('GBufferRT.guideNormalW', 'SVGFPass.WorldNormal')
+    g.add_edge('GBufferRT.mvec', 'SVGFPass.MotionVec')
     g.mark_output('ToneMapper.dst')
     return g
 
